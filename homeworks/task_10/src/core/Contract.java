caaf9a55304ae4646ea2893bf58fb12f8bf6dfdb
ledetav <nord.hilman@gmail.com>
2023-01-05 01:44:55 +0300

    add_addNewContractWithUniqueNumber ERROR

---
+++ homeworks/task_10/src/core/Contract.java
@@ -0,0 +1,22 @@
+package core;
+
+public class Contract {
+    private String number;
+    private String date;
+
+    public Contract(String number, String date) {
+        if (number == null || number.isEmpty()) {
+            throw new IllegalArgumentException("Contract number cannot be null or empty");
+        }
+        this.number = number;
+        this.date = date;
+    }
+
+    public String getNumber() {
+        return null;
+    }
+
+    public String getDate() {
+        return null;
+    }
+}
---
+++ homeworks/task_10/src/tests/ContractTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import core.Contract;
+
+public class ContractTest {
+    @Test
+    public void add_addNewContractWithUniqueNumber() {
+        String number = "123456";
+        String date = "20210101";
+        Contract contract1 = new Contract(number, date);
+        assertNotNull(contract1);
+        assertEquals(number, contract1.getNumber());
+        assertEquals(date, contract1.getDate());
+
+        String number2 = "654321";
+        Contract contract2 = new Contract(number2, date);
+        assertNotNull(contract2);
+        assertEquals(number2, contract2.getNumber());
+        assertEquals(date, contract2.getDate());
+    }
+}
